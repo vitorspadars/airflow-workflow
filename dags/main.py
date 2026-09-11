3e6778695be85de022c6f8367cee0a1b31bc24d8
vitorspada <vitorspada.rs@gmail.com>
2026-09-11 06:27:57 -0300

    teste

--- dags/main.py
+++ dags/main.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators import PythonOperator
+from airflow.providers.standard.operators.python import PythonOperator
 from datetime import datetime
 
 def helloWorld():
